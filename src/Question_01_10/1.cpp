d548467d94f5f81d0539cda3d47cbb90fc6cf674
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-04 04:58:53 +0900

    😊1_AC

--- src/Question_01_10/1.cpp
+++ src/Question_01_10/1.cpp
@@ -1,20 +1,46 @@
 // 2024/04/04
-//
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
+// 自力AC
+
+#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
 
 int main(int argc, const char *argv[])
 {
-    cv::Mat redImg(cv::Size(320, 240), CV_8UC3, cv::Scalar(0, 0, 255));
-    cv::namedWindow("red", cv::WINDOW_AUTOSIZE);
 
-    cv::imshow("red", redImg);
+    if (argc != 2)
+    {
+        cout << "usage" << endl;
+    }
+
+    Mat im_in = imread(argv[1], 1);
+    if (!im_in.data)
+    {
+        cout << "No image data" << endl;
+        return -1;
+    }
+    Mat im_out = im_in.clone();
+    int j_height = im_in.rows;
+    int i_width = im_in.cols;
+    int size = j_height * i_width;
+    for (int j = 0; j < j_height; j++)
+    {
+        for (int i = 0; i < i_width; i++)
+        {
+            im_out.at<Vec3b>(j, i)[0] = im_in.at<Vec3b>(j, i)[2];
+            im_out.at<Vec3b>(j, i)[1] = im_in.at<Vec3b>(j, i)[1];
+            im_out.at<Vec3b>(j, i)[2] = im_in.at<Vec3b>(j, i)[0];
+        }
+    }
+
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", im_out);
     cv::waitKey(0);
 
     cv::destroyAllWindows();
+    // save image
+    cv::imwrite("../output/1.bmp", im_out);
 
     return 0;
 }
